1f703d3a13584de22803e63de480930e829b50f4
Zac Hooper <zac.g.hooper@gmail.com>
2020-04-12 19:27:14 +0100

    Writes the list to a csv file

--- no-hip-hop-hitlist.py
+++ no-hip-hop-hitlist.py
@@ -1,6 +1,7 @@
 import spotipy
 from configs import config
 from spotipy.oauth2 import SpotifyClientCredentials
+import csv
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -18,15 +19,17 @@ artist1_id = playlists['items'][0]['track']['artists'][0]['id']
 def get_artist_genres(artist_id):
     artist = sp.artist(artist_id)
     return artist['genres']
-
-for item in playlists['items']:
-    track_name = item['track']['name']
-    artist_name = item['track']['artists'][0]['name']
-    artist_id = item['track']['artists'][0]['id']
-    artist_genres = get_artist_genres(artist_id)
-    print("Track: {} -- Artist: {} -- Genre: {}".format(track_name, artist_name, artist_genres))
-
-
+with open('hitlist.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Track', 'Artist', 'Genre'])
+    for item in playlists['items']:
+        track_name = item['track']['name']
+        artist_name = item['track']['artists'][0]['name']
+        artist_id = item['track']['artists'][0]['id']
+        artist_genres = get_artist_genres(artist_id)
+        print("Track: {} -- Artist: {} -- Genre: {}".format(track_name, artist_name, artist_genres))
+        spamwriter.writerow([track_name, artist_name, artist_genres])
 
 
 
