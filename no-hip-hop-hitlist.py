7bc1b07683e4ba33c33c84171a0b58b4850f53f6
Zac Hooper <zac.g.hooper@gmail.com>
2020-04-14 09:08:16 +0100

    Finished V1

--- no-hip-hop-hitlist.py
+++ no-hip-hop-hitlist.py
@@ -1,17 +1,29 @@
 import spotipy
 from configs import config
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
 import csv
 import fnmatch
 
+username = '1260769471'
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope)
+
 client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, auth=token)
 
 hitlist_id = '7vFQNWXoblEJXpbnTuyz76'
-test_id = '19fVvXqXq8KUpK3MtNIIxJ'
-genre_test_id = '5QWNXDGREE9hAxddXe2W3m'
+banned_songs_id = '5QWNXDGREE9hAxddXe2W3m'
+no_hiphop_hitlist = '1ahh5eiX08eeiKxxXqZlPp'
 
 playlists = sp.playlist_tracks(hitlist_id)
+banned_playlist = sp.playlist_tracks(banned_songs_id)
+banned_songs = [item['track']['id'] for item in banned_playlist['items']]
+
+def clean_playlist(playlist_id):
+    current_playlist = sp.playlist_tracks(playlist_id)
+    current_songs = [item['track']['id'] for item in current_playlist['items']]
+    sp.user_playlist_remove_all_occurrences_of_tracks(username, playlist_id, current_songs)
 
 # If returns True the artist is okay, if false remove, if -1 unknown
 def check_genre(artist_genres):
@@ -30,7 +42,6 @@ def check_genre(artist_genres):
     else:
         return False
 
-
 def get_artist_genres(artist_id):
     artist = sp.artist(artist_id)
     return artist['genres']
@@ -38,20 +49,31 @@ def get_artist_genres(artist_id):
 with open('hitlist.csv', 'w') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Track', 'Artist', 'Genre'])
+    spamwriter.writerow(['Track', 'Artist', 'Genre', 'Genre Status'])
     
+    clean_playlist(no_hiphop_hitlist)
     ok_list = []
 
     for item in playlists['items']:
         track_name = item['track']['name']
+        track_id = item['track']['id']
         artist_name = item['track']['artists'][0]['name']
         artist_id = item['track']['artists'][0]['id']
         artist_genres = get_artist_genres(artist_id)
+
         genre_status = check_genre(artist_genres)
-        print("Track: {} -- Artist: {} -- Genre: {} -- Genre Status: {}".format(track_name, artist_name, artist_genres, genre_status))
-        if not genre_status:
+
+        if not genre_status or track_id in banned_songs:
+            print("REMOVED____Track: {} -- Artist: {} -- Genre: {} -- Genre Status: {}".format(track_name, artist_name, artist_genres, genre_status))
             spamwriter.writerow([track_name, artist_name, artist_genres, genre_status])
             continue
-        ok_list.append(track_name)
 
+        ok_list.append(track_id)
 
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, no_hiphop_hitlist, ok_list)
+    print(results)
+else:
+    print("Can't get token for", username)
